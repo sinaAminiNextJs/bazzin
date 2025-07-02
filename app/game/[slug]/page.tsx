dd4c07a7d6bdac1340c063176c51a0771ebe8d6f
SinaAmini90 <siyam1990@gmail.com>
2025-07-02 14:11:10 +0330

    add game glug page

---
+++ app/game/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import BackButton from "@/app/components/BackButton";
+
+export default function GamePlay({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+
+  type gameType = {
+    name: string;
+    slug: string;
+    game: string;
+  };
+
+  const gameList: gameType[] = [
+    { name: "اسم بازی ۱", slug: "game-1", game: "/g1.png" },
+    { name: "اسم بازی ۲", slug: "game-2", game: "/g2.png" },
+    { name: "اسم بازی ۳", slug: "game-3", game: "/g3.png" },
+    { name: "اسم بازی ۴", slug: "game-4", game: "/g4.png" },
+    { name: "اسم بازی ۵", slug: "game-5", game: "/g5.png" },
+    { name: "اسم بازی ۶", slug: "game-6", game: "/g6.png" },
+  ];
+
+  const gameData: gameType | undefined = gameList.find(
+    (game) => game.slug === slug
+  );
+
+  return (
+    <section className="relative overflow-hidden w-full h-screen text-white min-h-screen flex flex-col items-center p-8 bg-mybg/96">
+      {/* page content */}
+      <h1 className="text-5xl font-madimi mt-10">BAZZIN</h1>
+      {gameData ? (
+        <section className="flex flex-col justify-center items-center h-full">
+          <img
+            src={gameData.game}
+            className="w-full aspect-square rounded-2xl"
+            alt={gameData.name}
+          />
+          <p className="text-center mt-4 text-3xl font-iranyekan">
+            {gameData.name}
+          </p>
+        </section>
+      ) : (
+        <section className="flex justify-center items-center h-full text-white">
+          <p className="text-xl font-iranyekan">بازی پیدا نشد.</p>
+        </section>
+      )}
+      {/* back button */}
+      <BackButton pathName="/game" />
+    </section>
+  );
+}
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
 import BackButton from "../components/BackButton";
 
-export default function About() {
+export default function Game() {
   type videoType = {
     name: string;
     game: string;
+    slug: string;
   };
 
   const videoList: videoType[] = [
-    { name: "اسم بازی", game: "/g1.png" },
-    { name: "اسم بازی", game: "/g2.png" },
-    { name: "اسم بازی", game: "/g3.png" },
-    { name: "اسم بازی", game: "/g4.png" },
-    { name: "اسم بازی", game: "/g5.png" },
-    { name: "اسم بازی", game: "/g6.png" },
-    { name: "اسم بازی", game: "/g1.png" },
-    { name: "اسم بازی", game: "/g2.png" },
-    { name: "اسم بازی", game: "/g3.png" },
-    { name: "اسم بازی", game: "/g4.png" },
+    { name: "اسم بازی", game: "/g1.png", slug: "game-1" },
+    { name: "اسم بازی", game: "/g2.png", slug: "game-2" },
+    { name: "اسم بازی", game: "/g3.png", slug: "game-3" },
+    { name: "اسم بازی", game: "/g4.png", slug: "game-4" },
+    { name: "اسم بازی", game: "/g5.png", slug: "game-5" },
+    { name: "اسم بازی", game: "/g6.png", slug: "game-6" },
+    { name: "اسم بازی", game: "/g1.png", slug: "game-1" },
+    { name: "اسم بازی", game: "/g2.png", slug: "game-2" },
+    { name: "اسم بازی", game: "/g3.png", slug: "game-3" },
+    { name: "اسم بازی", game: "/g4.png", slug: "game-4" },
   ];
 
   return (
@@ -38,14 +40,14 @@ export default function About() {
       <h1 className="text-5xl font-madimi mt-10">BAZZIN</h1>
       <p className="text-2xl mt-5">بازی و سرگرمی</p>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mt-10 text-lg leading-relaxed text-justify max-w-3xl mx-auto font-iranyekan">
-        {videoList.map(({ name, game }, index) => (
-          <article key={index} className="mx-auto ">
+        {videoList.map(({ name, game, slug }, index) => (
+          <Link href={`/game/${slug}`} key={index} className="mx-auto ">
             <img
               src={game}
               className="flex justify-center items-center w-32 aspect-square rounded-2xl"
             />
             <p className="text-center mt-2">{name}</p>
-          </article>
+          </Link>
         ))}
       </div>
       {/* back button */}
