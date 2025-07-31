76e188f7101de6cfb0fee92ff1e4840f486dbc35
SinaAmini90 <siyam1990@gmail.com>
2025-07-31 10:10:11 +0330

    fix bug "reticle" didint recognoze surfaces

--- app/earthThD/earthAR/components/AREarth.jsx
+++ app/earthThD/earthAR/components/AREarth.jsx
@@ -364,7 +364,6 @@
 // }
 ///////////////
 "use client";
-"use client";
 import { useEffect, useState, useRef } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
