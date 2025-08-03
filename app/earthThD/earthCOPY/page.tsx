c479a6fed31774fec5304650da208337ec1524ed
SinaAmini90 <siyam1990@gmail.com>
2025-08-03 15:15:14 +0330

    add some style

--- app/earthThD/earthCOPY/page.tsx
+++ app/earthThD/earthCOPY/page.tsx
@@ -14,28 +14,15 @@ export default function Page({ params }: any) {
   };
 
   return (
-    <main className="w-full flex flex-col bg-mybg/96 ">
-      {/* پس‌زمینه‌های تصویری */}
-      {/* <div className="absolute top-0 left-0 -z-10 w-full h-screen">
-        <img
-          src="/clipart/earth.png"
-          alt="Earth"
-          className="w-40 absolute top-20 -right-3"
-        />
-        <img
-          src="/clipart/earth.png"
-          alt="Earth"
-          className="w-96 absolute -bottom-7 -left-44"
-        />
-      </div> */}
+    <main className="w-full flex flex-col bg-mybg/96">
       {/* دکمه برگشت */}
       <BackButton pathName="/menu" />
 
-      <div className="flex flex-col px-3 pb-[80px] bg-mybg/96">
-        <div className="max-w-md mx-auto left-0 right-0 bottom-0 py-[15px] px-4 fixed  flex items-center justify-between z-[4] bg-res-50 bg-mybg/96">
+      <div className="bg-mybg/96">
+        <div className="mx-auto py-[15px] px-4 fixed  flex items-center justify-between bg-mybg/96">
           <button
             onClick={startModel}
-            className="w-[200px] h-[40px] rounded-full flex items-center justify-center text-white bg-res-100 text-sm"
+            className="w-fit px-4 text-black h-12 bg-myorange rounded-2xl border-2 border-myorangeLight font-iranyekan text-xl active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
           >
             View Ar
           </button>
@@ -46,15 +33,28 @@ export default function Page({ params }: any) {
           visibleModel ? "top-0 visible min-h-screen" : "top-[100%] invisible"
         }`}
       >
-        <div className="w-full h-[50px]  flex items-center justify-end px-3">
-          <button
-            onClick={() => {
-              setVisibleModal(false);
-            }}
+        <button
+          onClick={() => {
+            setVisibleModal(false);
+          }}
+          className="fixed bottom-4 left-4 flex justify-center items-center w-12 pr-1 aspect-square bg-myred rounded-full border-2 border-myorangeLight active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={4}
           >
-            کلوز{" "}
-          </button>
-        </div>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={4}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>{" "}
+        </button>
         {/* @ts-ignore */}
         <model-viewer
           ar
@@ -72,7 +72,7 @@ export default function Page({ params }: any) {
           <button
             slot="ar-button"
             id="ar-button"
-            className="fixed bottom-4 w-fit px-4 text-black h-12 bg-myorange rounded-2xl border-2 border-myorangeLight font-iranyekan text-xl active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
+            className="fixed bottom-4 w-fit -translate-x-5 px-4 text-black h-12 bg-myorange rounded-2xl border-2 border-myorangeLight font-iranyekan text-xl active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
           >
             شروع واقعیت افزوده
           </button>
