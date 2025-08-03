bffcadebf692dfadcadd667f05bcf8fdb48cef56
SinaAmini90 <siyam1990@gmail.com>
2025-08-03 16:26:19 +0330

    change model sorce

--- app/earthThD/page.tsx
+++ app/earthThD/page.tsx
@@ -74,7 +74,7 @@ export default function HomePage() {
         <model-viewer
           ar
           modes="scene-viewer quick-look webxr"
-          src={"/ar-earth/earth.glb"}
+          src={"/ar-earth/untitled.gltf"}
           ios-src={"/model/pizza.usdz"} // AR iOS
           touch-action="pan-y"
           reveal="manual"
