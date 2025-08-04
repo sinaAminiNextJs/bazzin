f43ea20412ec739df4bee78a8577bf468fa4d31f
SinaAmini90 <siyam1990@gmail.com>
2025-08-04 16:18:36 +0330

    add new path

--- app/AR/page.tsx
+++ app/AR/page.tsx
@@ -20,68 +20,69 @@ export default function AR() {
     link.click();
   }
   return (
-    <>
-      <button onClick={startModel}>کلیک کنید</button>
-      {/* AR */}
-      <div
-        className={`fixed left-0 right-0 bottom-0 mx-auto bg-white shadow transition-all z-[10] duration-500 flex flex-col ${
-          visibleModel ? "top-0 visible min-h-full" : "top-[100%] invisible"
-        }`}
-      >
-        <button
-          onClick={() => {
-            setVisibleModal(false);
-          }}
-          className="fixed bottom-4 left-4 flex justify-center items-center w-12 pr-1 aspect-square bg-myred rounded-full border-2 border-myorangeLight active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black] z-20"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-white"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={4}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={4}
-              d="M15 19l-7-7 7-7"
-            />
-          </svg>{" "}
-        </button>
-        {/* @ts-ignore */}
-        <model-viewer
-          ar
-          ar-modes="scene-viewer quick-look webxr"
-          src={"/ar-earth/m1/untitled.gltf"}
-          ios-src={"/model/pizza.usdz"} // AR iOS
-          touch-action="pan-y"
-          reveal="manual"
-          auto-rotate
-          shadow-intensity="1"
-          camera-controls
-          style={{ width: "100%", height: "100%" }}
-          scale={"0.01 0.01 0.01"}
-          ref={modelRef}
-          // ar-scale="fixed"
-          ar-placement="floor"
-          interaction-prompt="none"
-        >
-          <div className="controls glass">
-            <button onClick={() => exportGLB()}>Export GLB</button>
-          </div>
+    <p>hui</p>
+    // <>
+    //   <button onClick={startModel}>کلیک کنید</button>
+    //   {/* AR */}
+    //   <div
+    //     className={`fixed left-0 right-0 bottom-0 mx-auto bg-white shadow transition-all z-[10] duration-500 flex flex-col ${
+    //       visibleModel ? "top-0 visible min-h-full" : "top-[100%] invisible"
+    //     }`}
+    //   >
+    //     <button
+    //       onClick={() => {
+    //         setVisibleModal(false);
+    //       }}
+    //       className="fixed bottom-4 left-4 flex justify-center items-center w-12 pr-1 aspect-square bg-myred rounded-full border-2 border-myorangeLight active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black] z-20"
+    //     >
+    //       <svg
+    //         xmlns="http://www.w3.org/2000/svg"
+    //         className="h-6 w-6 text-white"
+    //         fill="none"
+    //         viewBox="0 0 24 24"
+    //         stroke="currentColor"
+    //         strokeWidth={4}
+    //       >
+    //         <path
+    //           strokeLinecap="round"
+    //           strokeLinejoin="round"
+    //           strokeWidth={4}
+    //           d="M15 19l-7-7 7-7"
+    //         />
+    //       </svg>{" "}
+    //     </button>
+    //     {/* @ts-ignore */}
+    //     <model-viewer
+    //       ar
+    //       ar-modes="scene-viewer quick-look webxr"
+    //       src={"/ar-earth/m1/untitled.gltf"}
+    //       ios-src={"/model/pizza.usdz"} // AR iOS
+    //       touch-action="pan-y"
+    //       reveal="manual"
+    //       auto-rotate
+    //       shadow-intensity="1"
+    //       camera-controls
+    //       style={{ width: "100%", height: "100%" }}
+    //       scale={"0.01 0.01 0.01"}
+    //       ref={modelRef}
+    //       // ar-scale="fixed"
+    //       ar-placement="floor"
+    //       interaction-prompt="none"
+    //     >
+    //       <div className="controls glass">
+    //         <button onClick={() => exportGLB()}>Export GLB</button>
+    //       </div>
 
-          <button
-            slot="ar-button"
-            id="ar-button"
-            className="fixed bottom-4 right-1/6 w-[220px] px-4 text-black h-12 bg-myorange rounded-2xl border-2 border-myorangeLight font-iranyekan text-xl active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
-          >
-            شروع واقعیت افزوده
-          </button>
-          {/* @ts-ignore */}
-        </model-viewer>
-      </div>
-    </>
+    //       <button
+    //         slot="ar-button"
+    //         id="ar-button"
+    //         className="fixed bottom-4 right-1/6 w-[220px] px-4 text-black h-12 bg-myorange rounded-2xl border-2 border-myorangeLight font-iranyekan text-xl active:translate-y-[2px] active:scale-95 transition-all duration-100 shadow-[0px_0px_20px_black]"
+    //       >
+    //         شروع واقعیت افزوده
+    //       </button>
+    //       {/* @ts-ignore */}
+    //     </model-viewer>
+    //   </div>
+    // </>
   );
 }
